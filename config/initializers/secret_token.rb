8b1ffd5de636b5e2c57cb3e0c2ffdcccc73a372f
Tom Brennan <tommybrennan@gmail.com>
2014-08-30 11:54:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DanceSchool::Application.config.secret_key_base = 'c7c7317d229c01931fc5804e59f58d022e124cfd98d35ff8114b7464857e5b047c1ffe43030b60f7d41acd5829f20a0f1c17aedd177770ec8238ce260cd8e2c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DanceSchool::Application.config.session_store :cookie_store, key: '_dance_school_session'
