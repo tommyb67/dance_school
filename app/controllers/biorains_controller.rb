91121ccc7ab25fdf92ad4e2a9bfb1abe9755f6cd
Tom Brennan <tommybrennan@gmail.com>
2014-08-30 17:09:50 -0400

    updated the biorain controller

--- app/controllers/biorains_controller.rb
+++ app/controllers/biorains_controller.rb
@@ -1,58 +1,37 @@
 class BiorainsController < ApplicationController
   before_action :set_biorain, only: [:show, :edit, :update, :destroy]
 
-  # GET /biorains
-  # GET /biorains.json
   def index
     @biorains = Biorain.all
   end
 
-  # GET /biorains/1
-  # GET /biorains/1.json
   def show
   end
 
-  # GET /biorains/new
   def new
     @biorain = Biorain.new
   end
 
-  # GET /biorains/1/edit
   def edit
   end
 
-  # POST /biorains
-  # POST /biorains.json
   def create
     @biorain = Biorain.new(biorain_params)
-
-    respond_to do |format|
-      if @biorain.save
-        format.html { redirect_to @biorain, notice: 'Biorain was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @biorain }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @biorain.errors, status: :unprocessable_entity }
-      end
+    if @biorain.save
+      redirect_to @biorain, notice: 'Biorain was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /biorains/1
-  # PATCH/PUT /biorains/1.json
   def update
-    respond_to do |format|
-      if @biorain.update(biorain_params)
-        format.html { redirect_to @biorain, notice: 'Biorain was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @biorain.errors, status: :unprocessable_entity }
-      end
+    if @biorain.update(biorain_params)
+      redirect_to @biorain, notice: 'Biorain was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /biorains/1
-  # DELETE /biorains/1.json
   def destroy
     @biorain.destroy
     respond_to do |format|
