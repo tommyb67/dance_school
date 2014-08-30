ede0a1eba4cfcf6b8efd28fc2008e40b5b652289
Tom Brennan <tommybrennan@gmail.com>
2014-08-30 12:05:16 -0400

    set up the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 DanceSchool::Application.routes.draw do
-  get "pages/home"
+
+  root "pages#home"
   get "pages/about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
