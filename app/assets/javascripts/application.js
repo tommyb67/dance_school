382c72a947c362a24de7b14f997f6fb27a91e081
Tom Brennan <tommybrennan@gmail.com>
2014-09-01 18:55:43 -0400

    working on fixing the collaspeable nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
 //= require jquery.turbolinks
+//= require turbolinks
 //= require masonry/jquery.masonry
 //= require_tree .
 
