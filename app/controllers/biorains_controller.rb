3d0ed5df9b47b25e55ebfe85ceb4d8be073fc9c5
Tom Brennan <tommybrennan@gmail.com>
2014-09-04 13:04:11 -0400

    Load biorains in reverse chronological order

--- app/controllers/biorains_controller.rb
+++ app/controllers/biorains_controller.rb
@@ -4,7 +4,7 @@ class BiorainsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @biorains = Biorain.all
+    @biorains = Biorain.all.order("created_at DESC")
   end
 
   def show
