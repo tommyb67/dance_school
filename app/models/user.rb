f8bdd64034ac4a28ec80bd7f18c9e244a2f99b2e
Tom Brennan <tommybrennan@gmail.com>
2014-11-08 13:11:17 -0500

    fixed users pictures association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :events
-  has_many :pictures, :through => :events
+  has_many :pictures
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
