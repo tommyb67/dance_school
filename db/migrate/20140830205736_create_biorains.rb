57044a5378df6e100dcc14864dd6aebfcf6927fb
Tom Brennan <tommybrennan@gmail.com>
2014-11-20 15:22:51 -0500

    cleaned up migrations for delopyment to heroku

--- db/migrate/20140830205736_create_biorains.rb
+++
@@ -1,9 +0,0 @@
-class CreatePictures < ActiveRecord::Migration
-  def change
-    create_table :pictures do |t|
-      t.string :description
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140831012205_add_user_id_to_biorains.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToPictures < ActiveRecord::Migration
-  def change
-    add_column :pictures, :user_id, :integer
-    add_index :pictures, :user_id
-  end
-end
