d5e724b20a0cd203e30dadaa372859f8a0ea4881
Tom Brennan <tommybrennan@gmail.com>
2014-11-23 16:12:55 -0500

    added the repeat method on the lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,13 @@
 class Lesson < ActiveRecord::Base
   has_many :dancers
+
+  def repeat!(num_times)
+    1.upto(num_times) do |i|
+      next_lesson = dup                 # self is a Lesson instance (the first lesson created by controller)
+      next_lesson.starts_at + i.weeks   # http://api.rubyonrails.org/classes/Numeric.html gives you .weeks
+      next_lesson.ends_at + i.weeks
+      next_lesson.save!
+    end
+  end
+
 end
